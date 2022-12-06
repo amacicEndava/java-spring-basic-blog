4cf1b10ab3f4b39075251459d0969071705e94a7
amacicEndava <118282611+amacicEndava@users.noreply.github.com>
2022-12-06 15:16:33 +0100

    second commit

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,28 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap map){
-        map.put("title", "Blog Post 1");
+        List<Post> posts = postRepository.getAllPosts();
+        map.put("posts", posts);
 
         return "home";
     }
